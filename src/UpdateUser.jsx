e94a2fc92b3a41ebc04265f288ddcc41b7d1423f
Ragnarloki <sivasankarswaminatha62@gmail.com>
2024-02-20 20:56:44 +0530

    commited

--- src/UpdateUser.jsx
+++ src/UpdateUser.jsx
@@ -4,33 +4,33 @@ import { useState,useEffect } from 'react'
 import axios from 'axios'
 
 function UpdateUser() {
-//   const {id}=useParams()
+  const {id}=useParams()
 
-//   const [name,setName]=useState()
-//   const [email,setEmail]=useState()
-//   const [age,setAge]=useState()
-//   const navigate=useNavigate()
+  const [name,setName]=useState()
+  const [email,setEmail]=useState()
+  const [age,setAge]=useState()
+  const navigate=useNavigate()
 
-//   useEffect(()=>{
-//     axios.get('http://localhost:3000/getUser/'+id)
-//     .then(result=>{console.log(result)
-//           setName(result.data.name)
-//           setEmail(result.data.email)
-//           setAge(result.data.age)
+  useEffect(()=>{
+    axios.get('http://localhost:3000/getUser/'+id)
+    .then(result=>{console.log(result)
+          setName(result.data.name)
+          setEmail(result.data.email)
+          setAge(result.data.age)
           
-//     })
-//     .catch(err=>console.log(err))
-// },[])
+    })
+    .catch(err=>console.log(err))
+},[])
 
-// const Update=(e)=>{
-//   e.preventDefault();
-//   axios.put("http://localhost:3000/updateUser/"+id, {name,email,age})
-//   .then(result=>{console.log(result)
-//       navigate('/')
-//   })
-//   .catch(err=>console.log(err))
+const Update=(e)=>{
+  e.preventDefault();
+  axios.put("http://localhost:3000/updateUser/"+id, {name,email,age})
+  .then(result=>{console.log(result)
+      navigate('/')
+  })
+  .catch(err=>console.log(err))
 
-// }
+}
 
 
   return (
